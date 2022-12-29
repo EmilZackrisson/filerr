efeeb407c07e265ac927aab01c5f0fb7ed472f57
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-12-29 18:58:07 +0100

    test server

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,12 @@ app.get('/notify/new', (req: Request, res: Response) => {
     res.send(JSON.stringify(json));
   });
 
+  app.get('/api/node/notify/new', (req: Request, res: Response) => {
+    const json = req.body;
+    console.log(json);
+    res.send(JSON.stringify(json));
+  });
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });

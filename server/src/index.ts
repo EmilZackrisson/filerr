538409c2c716c496a4d919d87324b5302faf81ae
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-12-29 19:25:04 +0100

    test discord send

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,49 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+const { webhookUrl } = require('./config.json');
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = 8000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Filerr Notification API');
+	// testSendDiscord();
+	res.send('Filerr Notification API');
 });
 
-app.get('/notify/new', (req: Request, res: Response) => {
-    const json = req.body;
-    console.log(json);
-    res.send(JSON.stringify(json));
-  });
-
-  app.get('/api/node/notify/new', (req: Request, res: Response) => {
-    const json = req.body;
-    console.log(json);
-    res.send(JSON.stringify(json));
-  });
+app.post('/api/node/notify/new', (req: Request, res: Response) => {
+	const json = req.body;
+	const message = 'New request to Filerr from ' + json.name + ' on ' + json.file;
+	sendDiscord(message);
+	res.send(JSON.stringify(json));
+});
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+function sendDiscord(message: string) {
+	var request = require('request');
+
+	request.post(
+		//First parameter API to make post request
+		webhookUrl,
+
+		//Second parameter DATA which has to be sent to API
+		{
+			json: {
+				username: 'Filerr',
+				avatar_url: '',
+				content: 'This is a test message'
+			}
+		},
+
+		//Thrid parameter Callack function
+		function (error: Error, response: Response, body: any) {
+			if (!error && response.statusCode == 201) {
+				console.log(body);
+			}
+		}
+	);
+}

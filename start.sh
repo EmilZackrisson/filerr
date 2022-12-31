35ef689ebb5eba9b29cc89fc7847bb1fda257c4b
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-12-31 12:55:00 +0100

    copy env

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ if [ "$1" == "dev" ]; then
     docker compose -f docker-compose-dev.yaml build
     docker compose -f docker-compose-dev.yaml down
     docker compose -f docker-compose-dev.yaml up -d
-# elif [ "$mode" = "prod" ]; then
-#     echo "Starting in prod mode"
-#     npm run prod
+# if [ "$1" = "down" ]; then
+#     echo "Stopping containers"
+#     docker compose -f docker-compose-dev.yaml down
 else
     echo "Invalid mode"
     echo "Mode: $mode"

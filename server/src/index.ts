99980ad0263d52fa5547694b1374d202873660e4
Emil Zackrisson <emil.zackrisson@gmail.com>
2023-01-03 15:33:23 +0100

    try catch server

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,17 +18,27 @@ const port = 8000;
 
 app.get('/', (req: Request, res: Response) => {
 	// testSendDiscord();
-	res.send('Filerr Notification API');
+	res.send('Filerr API');
+	res.sendStatus(200);
 });
 
 app.post('/api/node/notify/new', (req: Request, res: Response) => {
-	const json = req.body;
-	getExtraData(json.user).then((data) => {
-		const message = 'New request to Filerr from ' + data.name + ' on ' + json.file;
-		sendDiscord(message);
-	});
-	console.log(req.body);
-	res.send(JSON.stringify(json));
+	try {
+		const json = req.body;
+		getExtraData(json.user)
+			.then((data) => {
+				const message = 'New request to Filerr from ' + data.name + ' on ' + json.file;
+				sendDiscord(message);
+			})
+			.then(() => {
+				res.sendStatus(200);
+				res.send(JSON.stringify(json));
+			});
+		console.log(req.body);
+	} catch (error) {
+		res.sendStatus(500);
+		res.send(error);
+	}
 });
 
 app.listen(port, () => {
@@ -36,39 +46,51 @@ app.listen(port, () => {
 });
 
 async function getExtraData(userId: string) {
-	const userdata = await pb.collection('users').getOne(userId);
-	return userdata;
+	try {
+		const userdata = await pb.collection('users').getOne(userId);
+		return userdata;
+	} catch (error) {
+		console.log('Error getting extra data: ' + error);
+	}
 }
 
 async function authPocketbase() {
-	const authData = await pb.admins.authWithPassword(
-		process.env.POCKETBASE_ADMIN_USER!,
-		process.env.POCKETBASE_ADMIN_PASSWORD!
-	);
-	console.log('Authenticated as: ' + pb.authStore.model?.id);
+	try {
+		const authData = await pb.admins.authWithPassword(
+			process.env.POCKETBASE_ADMIN_USER!,
+			process.env.POCKETBASE_ADMIN_PASSWORD!
+		);
+		console.log('Authenticated as: ' + pb.authStore.model?.id);
+	} catch (error) {
+		console.log('Error authenticating: ' + error);
+	}
 }
 
 function sendDiscord(message: string) {
 	var request = require('request');
 
-	request.post(
-		//First parameter API to make post request
-		process.env.DISCORD_WEBHOOK_URL!,
+	try {
+		request.post(
+			//First parameter API to make post request
+			process.env.DISCORD_WEBHOOK_URL!,
 
-		//Second parameter DATA which has to be sent to API
-		{
-			json: {
-				username: 'Filerr',
-				avatar_url: '',
-				content: message
-			}
-		},
+			//Second parameter DATA which has to be sent to API
+			{
+				json: {
+					username: 'Filerr',
+					avatar_url: '',
+					content: message
+				}
+			},
 
-		//Thrid parameter Callack function
-		function (error: Error, response: Response, body: any) {
-			if (!error && response.statusCode == 201) {
-				console.log(body);
+			//Thrid parameter Callack function
+			function (error: Error, response: Response, body: any) {
+				if (!error && response.statusCode == 201) {
+					console.log(body);
+				}
 			}
-		}
-	);
+		);
+	} catch (error) {
+		console.log('Error sending discord message: ' + error);
+	}
 }

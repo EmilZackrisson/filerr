17d5c10dd05263bae90864f5f1e2ce5f18767434
Emil Zackrisson <emil.zackrisson@gmail.com>
2023-03-17 13:17:42 +0100

    dfhgdfg

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -41,6 +41,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
+	export const EFC_17912: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
@@ -164,6 +165,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
+		EFC_17912: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
